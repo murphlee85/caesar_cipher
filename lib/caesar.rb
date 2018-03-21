659794489757fc55aabdbc80400a493e8a4cb4b1
Charles Lee <pcrlee85@gmail.com>
2018-03-21 15:20:42 -0600

    create pull request

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,6 +1,18 @@
 class Caesar
-  attr_reader :key
 
   def initialize
+    @alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+  end
+
+  def eng_to_cipher (plaintext, shift)
+    plaintext.chars.map do |letter|
+      translate_letter(letter, shift)
+  end.join
+
+  def translate_letter(letter, shift)
+    return " " if letter == " "
+    rotated = @alphabet.rotate(shift * -1)
+    index = @alphabet.index(letter)
+    rotated[index]
   end
 end
--- test/caesar_test.rb
+++ test/caesar_test.rb
@@ -1,2 +1,31 @@
-# YOUR TESTS GOES HERE
-# I PITY THE FOOL WHO DOESNT WRITE TESTS
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/caesar'
+require 'pry'
+
+class CaesarTest < Minitest::Test
+  def test_caesar_exists
+    caesar = Caesar.new
+# binding.pry
+    assert_instance_of Caesar, caesar
+  end
+
+  def test_it_can_translate_a_letter
+    caesar = Caesar.new
+
+    assert_equal "x", caesar.translate_letter("a", 3)
+  end
+
+  def test_it_can_translate_a_different_letter
+    caesar = Caesar.new
+
+    assert_equal "a", caesar.translate_letter("d", 3)
+
+  def test_it_can_translate
+    caesar = Caesar.new
+
+    plaintext = "the quick brown fox jumps over the lazy dog"
+    result = caesar.eng_to_cipher(plaintext, 3)
+    expected = "qeb nrfzh yoltk clu grkmp lsbo qeb ixwv ald"
+
+    assert_equal expected, result
